1dc2af3a57293ab92eef12514bde536f95ff05d8
wkelly1 <william.kelly20@btinternet.com>
2017-10-10 21:19:46 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+@app.route('/<user>')
+def index(user=None):
+    return render_template("profile.html", user=user)
+
+
+if __name__ == "__main__":
+    app.run()

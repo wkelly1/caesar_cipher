8b9339aa95a1c0a4f13dc9feed5de68a2c05510e
wkelly1 <william.kelly20@btinternet.com>
2017-11-04 21:14:54 +0000

    changes

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def caesarcipher():
     try:
         if request.method == "POST":
             txt = request.form['unEncryptedTxt'].lower()
-            shift = 2#int(input("Shift amount: "))
+            shift = int(request.form['shift'])
             txtOut = ""
             alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
                         "t", "u", "v", "w", "x", "y", "z"]
@@ -25,7 +25,6 @@ def caesarcipher():
             output1=txtOut
 
             txt = request.form['encryptedTxt'].lower()
-            shift = 2  # int(input("Shift amount: "))
             txtOut = ""
             alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
                         "t", "u", "v", "w", "x", "y", "z"]

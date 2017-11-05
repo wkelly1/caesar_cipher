dc6df86f818560cf9da0c50bd4677391c596014f
wkelly1 <william.kelly20@btinternet.com>
2017-11-05 19:25:07 +0000

    changes

--- main.py
+++ main.py
@@ -83,8 +83,62 @@ def caesarcipher2():
 
 @app.route("/caesarcipherkeyed/", methods=["POST", "GET"])
 def caesarcipherkeyed():
+    try:
+        if request.method == "POST":
+            print(request.form['options'])
+            if request.form['options']=="encrypt":
+                txt = request.form['txt'].lower()
+                keyword = request.form['key'].lower()
+                shift = int(request.form['shift'])
+                txtOut = ""
 
-    return render_template('caesarcipherkeyed.html')
+                alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                            "t", "u", "v", "w", "x", "y", "z"]
+                keyword = ''.join(sorted(set(keyword), key=keyword.index))
+
+                for elmt in alphabet:
+                    for i in keyword:
+                        if elmt == i:
+                            alphabet.remove(i)
+
+                for elmt in alphabet:
+                    for i in keyword:
+                        if elmt == i:
+                            alphabet.remove(i)
+
+                for elmt in keyword[::-1]:
+                    alphabet.insert(0, elmt)
+
+                txtOut = ""
+
+                for i in txt:
+                    if i != " ":
+                        position = alphabet.index(i)
+                        position = (position + shift) % 26
+                        letter = alphabet[position]
+                        txtOut += (letter)
+
+
+                output = txtOut
+            elif request.form['options'] == "decrypt":
+
+                txt = request.form['txt'].lower()
+                shift = int(request.form['shift'])
+                txtOut = ""
+                alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+                            "t", "u", "v", "w", "x", "y", "z"]
+                for i in txt:
+                    if i != " ":
+                        position = alphabet.index(i)
+                        position = (position - shift) % 26
+                        letter = alphabet[position]
+                        txtOut += (letter)
+
+                output = txtOut
+
+        return render_template('caesarcipherkeyed.html', output=output)
+    except:
+        return render_template('caesarcipherkeyed.html')
 
 if __name__ == "__main__":
     app.run()
